cdedb8937327301fed6e65d6ee16bdab3c70653c
Malay <malaymridha008@gmail.com>
2024-02-14 19:06:35 +0530

    Python

---
+++ Python/17_tuples.py
@@ -0,0 +1,14 @@
+# tuple = collection which is ordered and unchangeable 
+#         used to group together related data
+
+student = ("student1", 21, "male")
+
+print(student.count("student1"))
+
+print(student.index("male"))
+
+for x in student:
+    print(x)
+
+if "student1" in student:
+    print("Student is here!")
